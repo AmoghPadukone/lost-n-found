a0d0fce20d7ace43e0e156707dc25f32b3217f74
Amogh <35802992+AmoghPadukone@users.noreply.github.com>
2023-06-28 14:39:54 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,13 +9,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAp8tiCo7S7o2tTpcClOPKS1oTc8c8yrnI",
-  authDomain: "lostnfound-2023.firebaseapp.com",
-  projectId: "lostnfound-2023",
-  storageBucket: "lostnfound-2023.appspot.com",
-  messagingSenderId: "934957552689",
-  appId: "1:934957552689:web:92eb5b2739dab085dd7110",
-  measurementId: "G-BPV34D2QNP",
+// Firebase config object here
 };
 
 // Initialize Firebase
